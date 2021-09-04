eff74f83b31803b7d61d7dad8a0a55594cc0773e
Alejandro Alzate <alejo.15975355@gmail.com>
2021-09-04 14:33:37 -0500

    Created class Consolidated

---
+++ inputsalejo.Common/Models/Consolidated.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inputsalejo.Common.Models
+{
+    class Consolidated
+    {
+
+        //Table Atributes
+        public int EmployeeId { get; set; }
+        public DateTime ConsolidatedDate { get; set; }
+        public int Minutes { get; set; }
+    }
+}
---
+++ inputsalejo.Functions/Functions/ScheduledFunction.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace inputsalejo.Functions.Functions
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static void Run(
+            [TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, 
+            ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
